4c3417c38e3fb9607c82603d5d709c79658c09e8
姚一然 <11967234+yao-yiran@user.noreply.gitee.com>
2025-11-20 02:12:17 +0800

    test1

--- src/DataExporter.java
+++ src/DataExporter.java
@@ -7,6 +7,13 @@ import java.util.List;
  */
 public class DataExporter {
 
+    public boolean exportDataFunction(List<String> intValue, String path) {
+        if (processedData == null || processedData.isEmpty()) {
+            System.out.println("❌ 无有效数据可导出");
+            return false;
+        }
+    }
+
     /**
      * 导出处理后的数据（模拟导出到文件）
      * @param processedData 处理后的数据（来自DataProcessor）
